bba7d4e36f46144d47d1c09e58d2eb317f605a3e
Steven Tribbitt II <stribbitt@wlapcs.org>
2017-04-01 15:59:37 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+    
+}
+
+function draw(){
+    fill(255,0,0);
+    rect(290,100,250,250);
+    
+    
+    ellipse(50,250,80,80);
+    line(0,0,600,400);
+     ellipse(50,250,80,80);
+    
+}
